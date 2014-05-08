e06297d5935d294a41f7d8d16d5f43543dcac5c7
chriszf <chriszf@gmail.com>
2014-05-08 16:47:45 -0700

    Messing up the code

--- Homework03/accounting.py
+++ Homework03/accounting.py
@@ -1,28 +1,19 @@
 def main():
     f = open("melon_logs.log")
-    # input = aggregate melons
-    
     melon_tallies = {"musk": 0, "honeydew": 0, "bitter": 0}
     for line in f:
         data = line.split(":")
         melon_type = data[3]
         melon_count = int(data[4])
-
         melon_tallies[melon_type] += melon_count
     f.close()
-
     total_revenue = 0
-    melon_prices = {
-            "musk": 1.15,
-            "honeydew": 2.30,
-            "bitter": 0.5
-            }
+    melon_prices = { "musk": 1.15, "honeydew": 2.30, "bitter": 0.5 }
     total_revenue = 0
     for melon_type, melon_count in melon_tallies:
         price = melon_prices[melon_type]
         revenue = price * melon_count
         total_revenue += revenue
-
     f = open("melon_logs.log")
     total_spend = 0
     for line in f:
@@ -30,7 +21,7 @@ def main():
         if data[0] == "SPEND":
             amt = float(data[1])
             total_spend += amt
-
+    f.close()
     total_income = total_revenue - total_spend
     if total_income <= 0:
         color = '\033[91m'
