364cbf39ab7c9c61bcbb37f8db28dfb5b2d5a98d
cdueltgen <cdueltgen@gmail.com>
2014-05-19 15:19:43 -0700

    adding temp homework solution

---
+++ Homework02/h2_answer.py
@@ -0,0 +1,21 @@
+get_data(file_name):
+    my_file = open(file_name)
+
+    for line in my_file:
+        words = line.split()
+        day = words[0]
+        melon = words[1]
+        status = words[2]
+        print day, melon, status
+    my_file.close()
+
+def main():
+    print "WEEK 1"
+    get_data("week1.csv")
+    print "WEEK 2"
+    get_data("week2.csv")
+    print "WEEK 3"
+    get_data("week3.csv")
+
+if __name__ == "__main__":
+    main()
