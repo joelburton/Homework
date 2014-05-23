7d449957b89e40e0da067a8b936026d8e946f8b8
cdueltgen <cdueltgen@gmail.com>
2014-05-23 16:04:40 -0700

    changed to work with new log files

--- Homework03/accounting.py
+++ Homework03/accounting.py
@@ -1,33 +1,36 @@
 def main():
-    f = open("melon_logs.log")
-    melon_tallies = {"musk": 0, "honeydew": 0, "bitter": 0}
+    print "******************************************"
+    f = open("orders_by_type.csv")
+    melon_tallies = {"Musk": 0, "Hybrid": 0, "Watermelon": 0, "Winter": 0}
     for line in f:
-        data = line.split(":")
-        melon_type = data[3]
-        melon_count = int(data[4])
+        data = line.split(",")
+        melon_type = data[1]
+        melon_count = int(data[2])
         melon_tallies[melon_type] += melon_count
     f.close()
+    melon_prices = { "Musk": 1.15, "Hybrid": 1.30, "Watermelon": 1.75, "Winter": 4.00 }
     total_revenue = 0
-    melon_prices = { "musk": 1.15, "honeydew": 2.30, "bitter": 0.5 }
-    total_revenue = 0
-    for melon_type, melon_count in melon_tallies:
+    for melon_type in melon_tallies:
         price = melon_prices[melon_type]
-        revenue = price * melon_count
+        revenue = price * melon_tallies[melon_type]
         total_revenue += revenue
-    f = open("melon_logs.log")
-    total_spend = 0
+        print "We sold %d %s melons at %0.2f each for a total of %0.2f" % (melon_tallies[melon_type], melon_type, price, revenue)
+    print "******************************************"
+    f = open("orders_with_sales.csv")
+    sales = [0, 0]
     for line in f:
-        data = line.split(":")
-        if data[0] == "SPEND":
-            amt = float(data[1])
-            total_spend += amt
-    f.close()
-    total_income = total_revenue - total_spend
-    if total_income <= 0:
-        color = '\033[91m'
+        data = line.split(",")
+        if data[1] == "0":
+            sales[0] += float(data[3])
+        else:
+            sales[1] += float(data[3])
+    print "Salespeople generated %0.2f in revenue." % sales[1]
+    print "Internet sales generated %0.2f in revenue." % sales[0]
+    if sales[1] > sales[0]:
+        print "Guess there's some value to those salespeople after all."
     else:
-        color = '\033[92m'
-    print "We made this much money:", color + "%f"%total_income + '\033[0m'
+        print "Time to fire the sales team! Online sales rule all!"
+    print "******************************************"
 
 
 if __name__ == "__main__":
