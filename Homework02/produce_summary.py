0866d3b24d1a5e16bb545179525922812fdaf136
Joel Burton <joel@joelburton.com>
2015-01-13 12:44:16 -0800

    Remove __name__ cruft; clean up print "\n", -> print

--- Homework02/produce_summary.py
+++ Homework02/produce_summary.py
@@ -1,52 +1,47 @@
-def main():
+# Day 1
+print "Day 1"
+my_file = open("um-deliveries-20140519.csv")
 
-    # Day 1
-    print "Day 1"
-    my_file = open("um-deliveries-20140519.csv")
+for line in my_file:
+    line = line.rstrip()
+    words = line.split(',')
+    
+    melon = words[0]
+    count = words[0]
+    amount = words[0]
+    
+    print "Delivered %s %ss for a total of: $%s" % (count, melon, amount)
+my_file.close()
+print
 
-    for line in my_file:
-        line = line.rstrip()
-        words = line.split(',')
-        
-        melon = words[0]
-        count = words[0]
-        amount = words[0]
-        
-        print "Delivered %s %ss for a total of: $%s" % (count, melon, amount)
-    my_file.close()
-    print "\n",
+# Day 2
+print "Day 2"
+my_file = open("um-deliveries-20140520.csv")
 
-    # Day 2
-    print "Day 2"
-    my_file = open("um-deliveries-20140520.csv")
+for line in my_file:
+    line = line.rstrip()
+    words = line.split(',')
+    
+    melon = words[0]
+    count = words[0]
+    amount = words[0]
+    
+    print "Delivered %s %ss for a total of: $%s" % (count, melon, amount)
+my_file.close()
+print
 
-    for line in my_file:
-        line = line.rstrip()
-        words = line.split(',')
-        
-        melon = words[0]
-        count = words[0]
-        amount = words[0]
-        
-        print "Delivered %s %ss for a total of: $%s" % (count, melon, amount)
-    my_file.close()
-    print "\n",
+# Day 3
+print "Day 3"
+my_file = open("um-deliveries-20140521.csv")
 
-    # Day 3
-    print "Day 3"
-    my_file = open("um-deliveries-20140521.csv")
-
-    for line in my_file:
-        line = line.rstrip()
-        words = line.split(',')
-        
-        melon = words[0]
-        count = words[0]
-        amount = words[0]
-        
-        print "Delivered %s %ss for a total of: $%s" % (count, melon, amount)
-    my_file.close()
-    print "\n",
-
-if __name__ == "__main__":
-    main()
+for line in my_file:
+    line = line.rstrip()
+    words = line.split(',')
+    
+    melon = words[0]
+    count = words[0]
+    amount = words[0]
+    
+    print "Delivered %s %ss for a total of: $%s" % (count, melon, amount)
+my_file.close()
+print
