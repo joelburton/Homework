95d825625f30a6af13f4140cde240f65e8ea4c02
Joel Burton <joel@joelburton.com>
2015-01-13 12:53:18 -0800

    Remove __name__ cruft; make vars more obscured

--- Homework03/accounting.py
+++ Homework03/accounting.py
@@ -1,37 +1,32 @@
-def main():
-    print "******************************************"
-    f = open("orders_by_type.csv")
-    melon_tallies = {"Musk": 0, "Hybrid": 0, "Watermelon": 0, "Winter": 0}
-    for line in f:
-        data = line.split(",")
-        melon_type = data[1]
-        melon_count = int(data[2])
-        melon_tallies[melon_type] += melon_count
-    f.close()
-    melon_prices = { "Musk": 1.15, "Hybrid": 1.30, "Watermelon": 1.75, "Winter": 4.00 }
-    total_revenue = 0
-    for melon_type in melon_tallies:
-        price = melon_prices[melon_type]
-        revenue = price * melon_tallies[melon_type]
-        total_revenue += revenue
-        print "We sold %d %s melons at %0.2f each for a total of %0.2f" % (melon_tallies[melon_type], melon_type, price, revenue)
-    print "******************************************"
-    f = open("orders_with_sales.csv")
-    sales = [0, 0]
-    for line in f:
-        data = line.split(",")
-        if data[1] == "0":
-            sales[0] += float(data[3])
-        else:
-            sales[1] += float(data[3])
-    print "Salespeople generated %0.2f in revenue." % sales[1]
-    print "Internet sales generated %0.2f in revenue." % sales[0]
-    if sales[1] > sales[0]:
-        print "Guess there's some value to those salespeople after all."
+print "******************************************"
+f = open("orders_by_type.csv")
+melon_tallies = {"Musk":0, "Hybrid":0, "Watermelon":0, "Winter": 0}
+for l in f:
+    data = l.split(",")
+    melon_type = data[1]
+    melon_count = int(data[2])
+    melon_tallies[melon_type] += melon_count
+f.close()
+melon_prices = { "Musk": 1.15, "Hybrid": 1.30, "Watermelon": 1.75, "Winter": 4.00 }
+total_revenue = 0
+for melon_type in melon_tallies:
+    price = melon_prices[melon_type]
+    revenue = price * melon_tallies[melon_type]
+    total_revenue += revenue
+    print "We sold %d %s melons at %0.2f each for a total of %0.2f" % (melon_tallies[melon_type], melon_type, price, revenue)
+print "******************************************"
+f = open("orders_with_sales.csv")
+sales = [0, 0]
+for line in f:
+    d = line.split(",")
+    if d[1] == "0":
+        sales[0] += float(d[3])
     else:
-        print "Time to fire the sales team! Online sales rule all!"
-    print "******************************************"
-
-
-if __name__ == "__main__":
-    main()
+        sales[1] += float(d[3])
+print "Salespeople generated %0.2f in revenue." % sales[1]
+print "Internet sales generated %0.2f in revenue." % sales[0]
+if sales[1] > sales[0]:
+    print "Guess there's some value to those salespeople after all."
+else:
+    print "Time to fire the sales team! Online sales rule all!"
+print "******************************************"
